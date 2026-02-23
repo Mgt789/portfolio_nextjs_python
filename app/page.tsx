491baa7f378539b2530d442deec04986af708bd4
chandanm <chandan.m@quadwave.com>
2026-02-23 15:04:50 +0530

    hero image added

--- app/page.tsx
+++ app/page.tsx
@@ -2,65 +2,57 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black container max-w-6xl mx-auto">
+      <div className="heroBackground">
+        <div className="flex absolute">
+          <Image
+            src="/images/me.webp"
+            alt="Me"
+            width={300}
+            height={300}
+            priority
+          />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="flex align-item-center">
+
+          <Image
+            src="/images/me-glow.webp"
+            alt="Me"
+            width={300}
+            height={300}
+            priority
+          />
+          <div>
+            <p className="text-2xl">A Designer who</p>
+
+            <h1 className="text-5xl tracking-tight lg:text-7xl font-semibold text-white leading-tight">
+              Judges a book
+              <br />
+              by its{" "}
+              <span className="relative inline-block">
+                {/* <Image
+                  src="/assets/circle.png"
+                  alt="Circle"
+                  width={200}
+                  height={200}
+                  className="absolute mt-2"
+                /> */}
+                <span className="bg-gradient-to-r from-violet-600 via-violet-400 to-violet-600 bg-clip-text text-transparent">
+                  cover
+                </span>
+              </span>
+              ...
+            </h1>
+
+            <p className="text-md text-white/80">
+              Because if the cover does not impress you what else can?
+            </p>
+          </div>
+
         </div>
-      </main>
+
+      </div>
     </div>
   );
- 
+
 }
